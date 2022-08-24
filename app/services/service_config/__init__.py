87a5f396bebee71016e01dc6b0c4614b76514e2a
Andrew <andrew.shatz@trix.solutions>
2022-08-24 10:56:43 -0500

    Misc - Service Config Service and Script (#1)
    * Script - service config initial commit
    
    * Gitignore include all environments
    
    * Requirements - add file
    
    * App - initial commit
    
    * Services - initial commit
    
    * Services config - Ouroboros
    
    * Service Config Service - initial model
    
    * Services - move method to top of model class
    
    * Gitignore - add vscode
    
    * Scripts - service config script refactor

---
+++ app/services/service_config/__init__.py
@@ -0,0 +1,111 @@
+from typing import List
+
+from schematics import Model, types as t
+
+class ServiceParameter(Model):
+    name = t.StringType(required=True)
+    type = t.StringType(required=True)
+    required = t.BooleanType(default=False, serialize_when_none=False)
+    default = t.StringType(default=None, serialize_when_none=False)
+
+class ServiceMethod(Model):
+    name = t.StringType(required=True)
+    params = t.ListType(t.ModelType(ServiceParameter), default=[], serialize_when_none=False)
+    returns = t.StringType(default=None)
+
+    def add_parameter(self, name: str, type: str, required: bool = False, default: str = None):
+        param = ServiceParameter({
+            'name': name,
+            'type': type
+        })
+        if required:
+            param.required = required
+        if default:
+            param.default = default
+
+        try:
+            self.params.append(param)
+        except AttributeError:
+            self.params = [param]
+        
+        return param
+
+class ServiceModel(Model):
+    name = t.StringType(required=True)
+    type = t.StringType(choices=['Model', 'Enum', 'MondayModel'], default='Model')
+
+class ServiceConfiguration(Model):
+    name = t.StringType(required=True)
+    type = t.StringType(choices=['default', 'inteface'], default='default')
+    module = t.StringType(required=True)
+    models = t.ListType(t.ModelType(ServiceModel), default=[], serialize_when_none=False)
+    init_params = t.ListType(t.ModelType(ServiceParameter, default=[]), serialize_when_none=False)
+    methods = t.ListType(t.ModelType(ServiceMethod), default=[], serialize_when_none=False)
+
+    def add_model(self, name: str, type: str = 'Model'):
+        model = ServiceModel({
+            'name': name,
+            'type': type
+        })
+
+        try:
+            self.models.append(model)
+        except AttributeError:
+            self.models = [model]
+
+    def add_parameter(self, name: str, type: str, required: bool = False, default: str = None):
+        param = ServiceParameter({
+            'name': name,
+            'type': type
+        })
+        if required:
+            param.required = required
+        if default:
+            param.default = default
+
+        try:
+            self.init_params.append(param)
+        except AttributeError:
+            self.init_params = [param]
+        
+        return param
+
+    def get_method(self, method_id):
+        try:
+            return [method for method in self.methods if method.name == method_id][0]
+        except IndexError:
+            return None
+
+    def add_method(self, name: str, params: List[ServiceParameter] = [], returns: str = None):
+        method = ServiceMethod({
+            'name': name,
+        })
+        if params:
+            method.params = params
+        if returns:
+            method.returns = returns
+        
+        try:
+            self.methods.append(method)
+        except AttributeError:
+            self.methods = [method]
+
+        return method
+
+
+class InterfaceServiceConfiguration(ServiceConfiguration):
+    implementations = t.ListType(t.ModelType(ServiceConfiguration))
+
+    def add_implementation(self, name: str, module: str, init_params: List[ServiceParameter] = []):
+        impl = ServiceConfiguration({
+            'name': name,
+            'module': module
+        })
+        impl.init_params = [ServiceParameter(param) for param in init_params]
+
+        try:
+            self.implementations.append(impl)
+        except AttributeError:
+            self.implementations = [impl]
+        
+        return impl
---
+++ scripts/service_config.py
@@ -0,0 +1,74 @@
+import os
+import yaml
+from typing import List
+from functools import wraps
+
+from schematics.exceptions import DataError
+
+import sys
+sys.path.append(os.getcwd())
+
+
+from app.services.service_config import ServiceConfiguration, InterfaceServiceConfiguration
+
+APP_FILE_PATH = '/Users/ashatz/Work/Code/entman-webhooks-api'
+SERVICES_FILE = 'app/services/services.yml'
+
+def get_service_configs():
+    with open(os.path.join(APP_FILE_PATH, SERVICES_FILE), 'r') as stream:
+        config_file = yaml.safe_load(stream)
+
+        service_configs = []
+        for service_config in config_file:
+            try:
+                service = ServiceConfiguration(service_config)
+                if service.type == 'interface':
+                    service = InterfaceServiceConfiguration(service_config)
+            except DataError as ex:
+                try:
+                    ex.errors['implementations']
+                    service = InterfaceServiceConfiguration(service_config)
+                except KeyError as ex:
+                    continue
+            service_configs.append(service)
+    return service_configs
+
+def save_service_configs(configs: List[ServiceConfiguration]):
+    with open(os.path.join(APP_FILE_PATH, SERVICES_FILE), 'w') as stream:
+        config_data = [config.to_primitive() for config in configs]
+        yaml.safe_dump(config_data, stream)
+
+def get_service_config(service_configs: List[ServiceConfiguration], service_id: str, type: str = 'default'):
+    try:
+        service = [config for config in service_configs if config.name == service_id][0]
+        return service
+    except IndexError:
+        None
+
+def new_service(name: str, type: str, module: str):
+    service = ServiceConfiguration({
+        'name': name,
+        'type': type,
+        'module': module
+    })
+    if type == 'interface':
+        service = InterfaceServiceConfiguration(service.to_primitive())
+
+    return service
+
+
+def execute(func):
+    @wraps(func)
+
+    def wrapper(*args, **kwargs):
+        services = get_service_configs()
+        func(services, *args, **kwargs)
+        save_service_configs(services)
+    return wrapper
+
+@execute
+def custom_workflow(services, *args, **kwargs):
+    # Add custom code here.
+    pass
+
+custom_workflow()
