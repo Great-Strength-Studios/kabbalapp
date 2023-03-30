6b13a2920fdc6edc2e8bcbd7bec08a6c493b2fac
AShatz <andrew.shatz@trix.solutions>
2023-03-30 07:20:09 -0700

    Core: add yaml configuration reader

--- app/core/config/yaml.py
+++ app/core/config/yaml.py
@@ -7,7 +7,7 @@ class YamlAppConfigurationReader(AppConfigurationReader):
     def __init__(self, app_config_filepath: str):
         super().__init__(app_config_filepath)
 
-    def load_config(self, **kwargs) -> AppConfiguration:
+    def load_config(self, app_name: str, **kwargs) -> AppConfiguration:
         with open(self.app_config_filepath) as stream:
             app_components = yaml.safe_load(stream)
-        return AppConfiguration(**app_components)
+        return AppConfiguration(app_name, **app_components[app_name])
