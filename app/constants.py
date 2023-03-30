2a6e7df745df76b8ca6db3fc593d8d7d82bc5dba
AShatz <andrew.shatz@trix.solutions>
2023-03-30 05:02:11 -0700

    Constants: App configuration file path

--- app/constants.py
+++ app/constants.py
@@ -4,8 +4,7 @@ APP_ENV = 'APP_ENV'
 DEFAULT_APP_ENV = 'prod'
 
 # Configuration file
-CONFIG_FILE_DIRECTORY = 'app/{}'
-APP_SCHEMA_FILE = 'app.yml'
+APP_CONFIGURATION_FILE = 'app/app.yml'
 
 # Configuration
 CONFIGS = 'configs' 
