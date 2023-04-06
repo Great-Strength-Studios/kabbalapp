a9151db2d6f30a430bd3ec8be25e5b16fa0d1c88
AShatz <andrew.shatz@trix.solutions>
2023-04-06 15:11:49 -0700

    Domain: yaml save_project app_key param

--- app/domain/app_project/yaml.py
+++ app/domain/app_project/yaml.py
@@ -15,9 +15,9 @@ class YamlAppProjectManager(AppProjectManager):
             app_projects = yaml.safe_load(stream)
         return AppProject(app_projects[app_key])
 
-    def save_project(self, app_project: AppProject):
+    def save_project(self, app_key: str, app_project: AppProject):
         with open(self.app_project_filepath) as stream:
             app_projects = yaml.safe_load(stream)
-        app_projects[app_project.name] = app_project.to_primitive()
+        app_projects[app_key] = app_project.to_primitive()
         with open(self.app_project_filepath, 'w') as stream:
             yaml.safe_dump(app_projects, stream)
