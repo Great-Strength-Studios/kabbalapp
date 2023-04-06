fffb7e63b1b5ec42863206c9a77fbeca4bfb6bd8
AShatz <andrew.shatz@trix.solutions>
2023-04-06 15:13:02 -0700

    modules: features: app_project: new_app_project

---
+++ app/modules/app_project/new_app_project.py
@@ -0,0 +1,19 @@
+from ...core import *
+from ...domain import *
+
+def handle(context: MessageContext):
+
+    # Get new app project request.
+    request: NewAppProject = context.data
+
+    # Get app project manager.
+    manager: AppProjectManager = context.services.app_project_manager()
+
+    # Create app project.
+    app_project = AppProject(request.to_primitive('app_project.map'))
+
+    # Save app project.
+    manager.save_project(request.app_key, app_project)
+
+    # Return app project.
+    return app_project
