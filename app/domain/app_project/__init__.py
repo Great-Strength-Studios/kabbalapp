11cb9767734657064307837aac61e2c24ff5ea5c
AShatz <andrew.shatz@trix.solutions>
2023-04-06 12:18:09 -0700

    Services: app project manager interface

--- app/domain/app_project/__init__.py
+++ app/domain/app_project/__init__.py
@@ -5,3 +5,11 @@ class AppProject(Model):
     name = t.StringType(required=True)
     app_directory = t.StringType(required=True)
     
+
+class AppProjectManager():
+
+    def __init__(self, app_project_filepath: str):
+        self.app_project_filepath = app_project_filepath
+
+    def load_project(self, app_key: str) -> AppProject:
+        pass
