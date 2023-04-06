2ec3cce0216547ccacd999c066c2772ee8980f8d
AShatz <andrew.shatz@trix.solutions>
2023-04-06 13:12:29 -0700

    Endpoint: add cmd arguments

--- app/endpoints/cmd/arguments.py
+++ app/endpoints/cmd/arguments.py
@@ -7,6 +7,10 @@ parser.add_argument('subcommand')
 parser.add_argument('--env', default='prod')
 parser.add_argument('--debug', action='store_true')
 
+parser.add_argument('-n', '--name')
+parser.add_argument('-k', '--app-key')
+parser.add_argument('-d', '--app-directory')
+
 args = parser.parse_args()
 args = vars(args)
 try:
