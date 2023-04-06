3077b9f74fa2335554a044683d36928f434089a2
AShatz <andrew.shatz@trix.solutions>
2023-04-06 13:06:39 -0700

    Data mapping: new app project

--- app/core/mappings/data.py
+++ app/core/mappings/data.py
@@ -1,1 +1,8 @@
 from ..events import *
+
+def new_app_project(context, request, app_context, **kwargs):
+    return NewAppProject({
+        'name': request.get('name', None),
+        'app_key': request.get('app_key', None),
+        'app_directory': request.get('app_directory', None)
+    })
