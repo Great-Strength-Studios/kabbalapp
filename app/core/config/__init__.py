42d10263a3a10bb324fa7d7954f82c075440bc1f
AShatz <andrew.shatz@trix.solutions>
2023-04-06 15:39:30 -0700

    Config: break out module and feature configs

--- app/core/config/__init__.py
+++ app/core/config/__init__.py
@@ -1,45 +1,44 @@
 import os
 from schematics import types as t, Model
 
+class FeatureConfiguration(Model):
+        def __init__(self, raw_data: dict = None, **kwargs):
+            # Account for single-module configuration
+            try:
+                config = {}
+                config['modules'] = [{
+                    'function_path': raw_data.pop('module_path'),
+                    'params': raw_data.pop('params', {}),
+                    'data_mapping': raw_data.pop('data_mapping', None),
+                    'use_services': raw_data.pop('use_services', None),
+                    'log_activity': raw_data.pop('log_activity', True)
+                }]
+                config['header_mapping'] = raw_data.pop(
+                        'header_mapping', None)
+                super().__init__(raw_data=config)
+            except KeyError:
+                super().__init__(raw_data=raw_data)
+
+        class FunctionConfiguration(Model):
+            function_path = t.StringType(required=True)
+            data_mapping = t.StringType()
+            use_services = t.StringType()
+            params = t.DictType(t.StringType(), default={})
+            log_activity = t.BooleanType(default=True)
+
+        header_mapping = t.StringType()
+        functions = t.ListType(t.ModelType(FunctionConfiguration), default=[])
+        log_params = t.DictType(t.StringType(), default={})
+
+
+class ModuleConfiguration(Model):
+    features = t.DictType(t.ModelType(FeatureConfiguration), default={})
+
+
 class AppConfiguration(Model):
-
-    class ModuleConfiguration(Model):
-
-        class FeatureConfiguration(Model):
-            
-            def __init__(self, raw_data: dict = None, **kwargs):
-                # Account for single-module configuration
-                try:
-                    config = {}
-                    config['modules'] = [{
-                        'function_path': raw_data.pop('module_path'),
-                        'params': raw_data.pop('params', {}),
-                        'data_mapping': raw_data.pop('data_mapping', None),
-                        'use_services': raw_data.pop('use_services', None),
-                        'log_activity': raw_data.pop('log_activity', True)
-                    }]
-                    config['header_mapping'] = raw_data.pop('header_mapping', None)
-                    super().__init__(raw_data=config)
-                except KeyError:
-                    super().__init__(raw_data=raw_data)
-
-            class FunctionConfiguration(Model): 
-                function_path = t.StringType(required=True)
-                data_mapping = t.StringType()
-                use_services = t.StringType()
-                params = t.DictType(t.StringType(), default={})
-                log_activity = t.BooleanType(default=True)
-
-
-            header_mapping = t.StringType()
-            functions = t.ListType(t.ModelType(FunctionConfiguration), default=[])
-            log_params = t.DictType(t.StringType(), default={})
-
-
-        features = t.DictType(t.ModelType(FeatureConfiguration), default={})
-
     errors = t.DictType(t.StringType, default={})
     modules = t.DictType(t.ModelType(ModuleConfiguration), default={}, serialize_when_none=False)
+
 
 class AppConfigurationReader():
 
@@ -48,7 +47,8 @@ class AppConfigurationReader():
 
     def load_config(self, app_name: str, **kwargs) -> AppConfiguration:
         app_name, kwargs
-        pass 
+        pass
+
 
 def load_app_config_reader(app_config_filepath: str) -> AppConfigurationReader:
     if os.path.splitext(app_config_filepath)[1] in ['.yaml', '.yml']:
