1491fc9036dab1b6e4aeea78a6c137451cc6cee9
AShatz <andrew.shatz@trix.solutions>
2023-04-06 12:00:40 -0700

    Events: new app project event

--- app/core/events.py
+++ app/core/events.py
@@ -1,4 +1,9 @@
-from schematics import Model
+from schematics import types as t, Model
 
 class RequestEvent(Model):
     pass
+
+class NewAppProject(RequestEvent):
+    name = t.StringType(required=True)
+    app_key = t.StringType(required=True)
+    app_directory = t.StringType(required=True)
