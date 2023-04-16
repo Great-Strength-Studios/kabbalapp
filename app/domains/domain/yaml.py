d436d772fa6806c423f9ba2ea371155498c86627
ashatz <andrew@greatstrength.me>
2023-04-15 22:00:41 -0700

    Add yaml domain service

---
+++ app/domains/domain/yaml.py
@@ -0,0 +1,15 @@
+import yaml
+
+from . import *
+
+class YamlDomainService(DomainService):
+
+    def __init__(self, filepath: str):
+        self.filepath = filepath
+
+    def add_domain(self, domain_name: str):
+        with open(self.filepath, 'r') as f:
+            data = yaml.safe_load(f)
+        data['domains'].append(domain_name)
+        with open(self.filepath, 'w') as f:
+            yaml.dump(data, f)
