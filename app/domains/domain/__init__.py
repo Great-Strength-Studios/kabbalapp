f7f1fdc1fa39abf2c2efde93e9ee1e59e4d73ab6
ashatz <andrew@greatstrength.me>
2023-04-15 21:55:23 -0700

    Add domain entity

--- app/domains/domain/__init__.py
+++ app/domains/domain/__init__.py
@@ -1,4 +1,12 @@
 from schematics import types as t, Model
 
+### Section - Domain Models ###
 class Domain(Model):
     name = t.StringType(required=True)
+
+
+### Section - Domain Entities ###
+class DomainEntity(Domain):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
