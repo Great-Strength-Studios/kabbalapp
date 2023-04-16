90f3ed4e275c114c899f7aad4631a5a998582787
ashatz <andrew@greatstrength.me>
2023-04-16 14:05:58 -0700

    Add domain model to yaml domain service

--- app/domains/domain/yaml.py
+++ app/domains/domain/yaml.py
@@ -25,4 +25,24 @@ class YamlDomainService(DomainService):
             'key': key,
             'name': name
         }
-        return DomainEntity(raw_data=raw_data)
+        return DomainEntity(domain_service=self, raw_data=raw_data)
+    
+    def add_domain_model(self, domain_key: str, key: str, name: str) -> DomainModelEntity:
+        filepath = os.path.join(self.app_project.app_directory, APP_CONFIGURATION_FILE)
+        with open(filepath, 'r') as f:
+            data = yaml.safe_load(f)
+        if data['domains'] is not None and domain_key in data['domains']:
+            if data['domains'][domain_key]['models'] is not None and key in data['domains'][domain_key]['models']:
+                return ('DOMAIN_MODEL_ALREADY_EXISTS', key)
+            if data['domains'][domain_key]['models'] is None:
+                data['domains'][domain_key]['models'] = {key: {'name': name}}
+            else:
+                data['domains'][domain_key]['models'][key] = {'name': name}
+        else:
+            return ('DOMAIN_NOT_FOUND', domain_key)
+        with open(filepath, 'w') as f:
+            yaml.dump(data, f)
+        raw_data = {
+            'name': name
+        }
+        return DomainModelEntity(domain_service=self, raw_data=raw_data)
