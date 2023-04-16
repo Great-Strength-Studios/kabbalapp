8a7c2ca28c81a09795dde8064d372136cf183636
ashatz <andrew@greatstrength.me>
2023-04-15 21:58:05 -0700

    Add domain service

--- app/domains/domain/__init__.py
+++ app/domains/domain/__init__.py
@@ -1,12 +1,18 @@
 from schematics import types as t, Model
 
-### Section - Domain Models ###
+### Section - Models ###
 class Domain(Model):
     name = t.StringType(required=True)
 
 
-### Section - Domain Entities ###
+### Section - Entities ###
 class DomainEntity(Domain):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+### Section - Services ###
+class DomainService():
+
+    def add_domain(self, domain_name: str):
+        pass
