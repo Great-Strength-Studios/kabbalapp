38898df9d91e42a157240e5ae09f138230a10cbc
ashatz <andrew@greatstrength.me>
2023-04-16 06:57:45 -0700

    Update add domain function

--- app/modules/domains/add_domain.py
+++ app/modules/domains/add_domain.py
@@ -2,12 +2,20 @@ from ...core import *
 from ...domains import *
 
 def handle(context: MessageContext):
+    import os
 
     # Get request event.
     request: AddDomain = context.data
 
+    # Get app project key.
+    app_key = context.headers.get('app_key', None)
+
+    # Raise app error if app key is null.
+    if not app_key:
+        raise AppError(context.errors.APP_KEY_REQUIRED)
+
     # Get domain service.
-    service: DomainService = context.services.domain_service()
+    service: DomainService = context.services.domain_service(app_key)
 
     # Create domain key if none exists.
     if not request.key:
