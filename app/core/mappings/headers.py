c6564e836ebd2e03bbf4c504a70a440d58c49b72
ashatz <andrew@greatstrength.me>
2023-04-15 23:40:52 -0700

    Update headers for app key methods

--- app/core/mappings/headers.py
+++ app/core/mappings/headers.py
@@ -1,2 +1,24 @@
 def default(request, app_context, **kwargs):
-    return {}
+    # Get app key.
+    app_key = request.get('app_key', None)
+    
+    # Return if app key is null.
+    if not app_key:
+        return
+
+    # Get app project manager.
+    from ...domains import AppProjectManager
+    manager: AppProjectManager = app_context.container.app_project_manager()
+
+    # Get app project.
+    app_project = manager.load_project(app_key)
+
+    # Return header data.
+    return {
+        'app_key': app_key,
+        'app_directory': app_project.app_directory,
+        'app_name': app_project.name,
+        'app_version': app_project.version
+    }
+
+    
