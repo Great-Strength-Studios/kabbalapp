d00c439ecb1b5445564b961f90f604d681df00e1
ashatz <andrew@greatstrength.me>
2023-04-16 07:39:33 -0700

    Update add domain domain logic

--- app/domains/domain/yaml.py
+++ app/domains/domain/yaml.py
@@ -9,15 +9,20 @@ class YamlDomainService(DomainService):
     def __init__(self, app_project: AppProject):
         self.app_project = app_project
 
-    def add_domain(self, domain_key: str, domain_name: str) -> DomainEntity:
+    def add_domain(self, key: str, name: str) -> DomainEntity:
         filepath = os.path.join(self.app_project.app_directory, APP_CONFIGURATION_FILE)
         with open(filepath, 'r') as f:
             data = yaml.safe_load(f)
-        data['domains'].append({domain_key: {'name': domain_name}})
-        with open(self.filepath, 'w') as f:
+        if data['domains'] is not None and key in data['domains']:
+            return ('DOMAIN_ALREADY_EXISTS', key)
+        if data['domains'] is None:
+            data['domains'] = {key: {'name': name}}
+        else:
+            data['domains'][key] = {'name': name}
+        with open(filepath, 'w') as f:
             yaml.dump(data, f)
         raw_data = {
-            'key': domain_key,
-            'name': domain_name
+            'key': key,
+            'name': name
         }
         return DomainEntity(raw_data=raw_data)
