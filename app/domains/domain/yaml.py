f2c3e159ea92e5cd4c1903f3de903be8a344715e
ashatz <andrew@greatstrength.me>
2023-04-15 23:31:04 -0700

    Update yaml domain service add domain

--- app/domains/domain/yaml.py
+++ app/domains/domain/yaml.py
@@ -7,9 +7,14 @@ class YamlDomainService(DomainService):
     def __init__(self, filepath: str):
         self.filepath = filepath
 
-    def add_domain(self, domain_name: str) -> DomainEntity:
+    def add_domain(self, domain_key: str, domain_name: str) -> DomainEntity:
         with open(self.filepath, 'r') as f:
             data = yaml.safe_load(f)
-        data['domains'].append({'name': domain_name})
+        data['domains'].append({domain_key: {'name': domain_name}})
         with open(self.filepath, 'w') as f:
             yaml.dump(data, f)
+        raw_data = {
+            'key': domain_key,
+            'name': domain_name
+        }
+        return DomainEntity(raw_data=raw_data)
