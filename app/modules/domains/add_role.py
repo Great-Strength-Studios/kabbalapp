3ad9b9d3c9d9a513a94f94365153e670bb481b48
ashatz <andrew@greatstrength.me>
2023-04-16 20:48:03 -0700

    Module feature - add role

---
+++ app/modules/domains/add_role.py
@@ -0,0 +1,26 @@
+from ...core import *
+
+def handle(context: MessageContext):
+
+    # Unpack request.
+    request: AddDomainRole = context.data
+
+    # Retreive app key from headers.
+    app_key = context.headers.get('app_key', None)
+    if app_key is None:
+        raise AppError(context.errors.APP_KEY_REQUIRED)
+    
+    # Import domain.
+    from ...domains import domain as dom
+
+    # Get domain service.
+    domain_service: dom.DomainService = context.services.domain_service(app_key)
+
+    # Get domain.
+    domain: dom.DomainEntity = dom.get_domain(domain_service, request.domain_key)
+
+    # Add role.
+    role = domain.add_role(request.key, request.type, request.fields)
+
+    # Return response.
+    return role
