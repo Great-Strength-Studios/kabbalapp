543cafb76daf4d9a3cb0522051eee168f4684751
ashatz <andrew@greatstrength.me>
2023-04-16 17:03:51 -0700

    Create domain entities module

---
+++ app/domains/domain/entities.py
@@ -0,0 +1,22 @@
+from .core import *
+
+class DomainModelEntity(DomainModel):
+    def __init__(self, domain_service: DomainService,  **kwargs):
+        self.domain_service = domain_service
+        super().__init__(**kwargs)
+
+class DomainEntity(Domain):
+
+    def __init__(self, domain_service: DomainService,  **kwargs):
+        self.domain_service = domain_service
+        super().__init__(**kwargs)
+
+    def add_model(self, key: str, name: str) -> DomainModelEntity:
+        model = self.domain_service.add_domain_model(self.key, key, name)
+        return DomainModelEntity(domain_service=self.domain_service, raw_data=model.to_primitive())
+    
+def add_domain(domain_service: DomainService, domain_key: str, domain_name: str):
+    domain = domain_service.add_domain(domain_key, domain_name)
+    if isinstance(domain, tuple):
+        return domain
+    return DomainEntity(domain_service=domain_service, raw_data=domain.to_primitive())
