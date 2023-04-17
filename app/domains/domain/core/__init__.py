16b8c58539bef961cf434d65342683080861c84e
ashatz <andrew@greatstrength.me>
2023-04-16 17:03:39 -0700

    Remove old setup

--- app/domains/domain/core/__init__.py
+++
@@ -1,3 +0,0 @@
-from .models import Domain, DomainModel
-from .services import DomainService
-from .entities import DomainEntity, DomainModelEntity
--- app/domains/domain/core/models.py
+++
@@ -1,8 +0,0 @@
-from schematics import types as t, Model
-
-class Domain(Model):
-    key = t.StringType()
-    name = t.StringType(required=True)
-
-class DomainModel(Model):
-    name = t.StringType(required=True)
