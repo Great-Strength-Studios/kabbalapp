d5470e15789554f1506b35eb1422a9f739c1d1e7
ashatz <andrew@greatstrength.me>
2023-04-16 19:21:28 -0700

    yaml domain service - add domain model - fix bug

--- app/domains/domain/yaml.py
+++ app/domains/domain/yaml.py
@@ -40,22 +40,27 @@ class YamlDomainService(DomainService):
         else:
             return ('DOMAIN_NOT_FOUND', key)
     
-    def add_domain_model(self, domain_key: str, key: str, name: str) -> DomainModel:
+    def add_domain_model(self, domain_key: str, key: str, name: str, class_name: str) -> DomainModel:
         filepath = os.path.join(self.app_project.app_directory, APP_CONFIGURATION_FILE)
         with open(filepath, 'r') as f:
             data = yaml.safe_load(f)
+        model_data = {'name': name, 'class_name': class_name}
         if data['domains'] is not None and domain_key in data['domains']:
-            if data['domains'][domain_key]['models'] is not None and key in data['domains'][domain_key]['models']:
-                return ('DOMAIN_MODEL_ALREADY_EXISTS', key)
-            if data['domains'][domain_key]['models'] is None:
-                data['domains'][domain_key]['models'] = {key: {'name': name}}
-            else:
-                data['domains'][domain_key]['models'][key] = {'name': name}
+            try:
+                if data['domains'][domain_key]['models'] is not None and key in data['domains'][domain_key]['models']:
+                    return ('DOMAIN_MODEL_ALREADY_EXISTS', key)
+                if data['domains'][domain_key]['models'] is None:
+                    data['domains'][domain_key]['models'] = {key: model_data}
+                else:
+                    data['domains'][domain_key]['models'][key] = model_data
+            except KeyError:
+                data['domains'][domain_key]['models'] = {key: model_data}
         else:
             return ('DOMAIN_NOT_FOUND', domain_key)
         with open(filepath, 'w') as f:
             yaml.dump(data, f)
         raw_data = {
-            'name': name
+            'name': name,
+            'class_name': class_name
         }
         return DomainModel(raw_data=raw_data)
