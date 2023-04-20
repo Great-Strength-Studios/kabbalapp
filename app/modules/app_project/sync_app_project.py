a04c1471fd771308ff907493bcc7fd7b632b2f06
ashatz <andrew@greatstrength.me>
2023-04-19 20:22:26 -0700

    Simply load app for now.

--- app/modules/app_project/sync_app_project.py
+++ app/modules/app_project/sync_app_project.py
@@ -12,20 +12,5 @@ def handle(context: MessageContext):
     # Load target app printer.
     target_app_printer: AppPrinter = context.services.app_printer(request.app_key)
 
-    # Arrange packages/modules to be read.
-    modules = [
-        'app/core/config/__init__.py',
-        'app/core/config/json.py',
-        'app/core/config/yaml.py',
-        'app/core/error.py',
-        'app/core/routing.py',
-        'app/core/__init__.py',
-        'app/__init__.py'
-    ]
-
-    # Load blocks.
-    blocks = [app_printer.read_block(module) for module in modules]
-
-    # Write blocks.
-    for block in blocks:
-        target_app_printer.print_block(block)
+    # Load app.
+    app = app_printer.load_app()
