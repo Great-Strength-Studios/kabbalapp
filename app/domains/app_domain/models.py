208542a74fb62ce080d0ec3398b2275239486e27
ashatz <andrew@greatstrength.me>
2023-04-23 13:58:28 -0700

    Add roles and models to app domain

--- app/domains/app_domain/models.py
+++ app/domains/app_domain/models.py
@@ -3,14 +3,15 @@ from schematics import types as t, Model
 
 from ...constants import *
 
-class AppDomain(Model):
-    key = t.StringType()
-    name = t.StringType(required=True)
-
 class AppDomainModel(Model):
     name = t.StringType(required=True)
-    class_name = t.StringType(required=True)
 
 class AppDomainRole(Model):
     type = t.StringType(required=True, choices=DOMAIN_ROLE_TYPES)
     fields = t.ListType(t.StringType(), required=True)
+
+class AppDomain(Model):
+    key = t.StringType()
+    name = t.StringType(required=True)
+    roles = t.DictType(t.ModelType(AppDomainRole), default={})
+    models = t.DictType(t.ModelType(AppDomainModel), default={})
