c41c5afeaaaf0df138f3dbf5c3100f2e0e3ec2f4
ashatz <andrew@greatstrength.me>
2023-04-23 14:04:20 -0700

    Add domain property and role types.

--- app/domains/app_domain/models.py
+++ app/domains/app_domain/models.py
@@ -1,7 +1,31 @@
 from typing import List
 from schematics import types as t, Model
 
-from ...constants import *
+DOMAIN_ROLE_TYPES = [
+    'whitelist',
+    'blacklist'
+]
+DOMAIN_PROPERTY_TYPES = [
+    'str',
+    'int',
+    'float',
+    'bool',
+    'datetime',
+    'date',
+    'time',
+    'list',
+    'dict',
+    'model'
+]
+
+class AppDomainModelProperty(Model):
+    name: t.StringType(required=True)
+    type: t.StringType(required=True, choices=DOMAIN_PROPERTY_TYPES)
+    required: t.BooleanType(default=False)
+    default: t.StringType()
+    choices: t.ListType(t.StringType())
+    serialized_name = t.StringType()
+    deserialize_from = t.ListType(t.StringType(), default=[])
 
 class AppDomainModel(Model):
     name = t.StringType(required=True)
@@ -10,8 +34,9 @@ class AppDomainRole(Model):
     type = t.StringType(required=True, choices=DOMAIN_ROLE_TYPES)
     fields = t.ListType(t.StringType(), required=True)
 
+
 class AppDomain(Model):
     key = t.StringType()
     name = t.StringType(required=True)
-    roles = t.DictType(t.ModelType(AppDomainRole), default={})
+    roles = t.ListType(t.ModelType(AppDomainRole), default=[])
     models = t.DictType(t.ModelType(AppDomainModel), default={})
