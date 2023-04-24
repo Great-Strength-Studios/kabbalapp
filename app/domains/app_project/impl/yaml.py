bf78ecede1f44d7dbf7370a687e22032d059c7cc
ashatz <andrew@greatstrength.me>
2023-04-24 05:42:36 -0700

    Update service impl methods

--- app/domains/app_project/impl/yaml.py
+++ app/domains/app_project/impl/yaml.py
@@ -9,19 +9,23 @@ class YamlAppProjectManager(AppProjectManager):
         if not os.path.exists(self.app_project_filepath):
             os.makedirs(os.path.dirname(self.app_project_filepath), exist_ok=True)
             with open(self.app_project_filepath, 'w') as stream:
-                stream.write('')
+                data = {'projects': None, 'default_project': None}
+                yaml.safe_dump(data, stream)
 
     def load_project(self, app_key: str) -> AppProject:
         with open(self.app_project_filepath) as stream:
             app_projects = yaml.safe_load(stream)
-        return AppProject(app_projects[app_key])
+        try:
+            return AppProject(app_projects['projects'][app_key])
+        except KeyError:
+            return None
 
     def save_project(self, app_key: str, app_project: AppProject):
         with open(self.app_project_filepath) as stream:
             app_projects = yaml.safe_load(stream)
         try:
-            app_projects[app_key] = app_project.to_primitive()
+            app_projects['projects'][app_key] = app_project.to_primitive()
         except TypeError:
-            app_projects = {app_key: app_project.to_primitive()}
+            app_projects['projects'] = {app_key: app_project.to_primitive()}
         with open(self.app_project_filepath, 'w') as stream:
             yaml.safe_dump(app_projects, stream)
