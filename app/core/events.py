f3c3dab73a0aca25139b2ece1bd286d2c377c041
ashatz <andrew@greatstrength.me>
2023-04-24 06:10:32 -0700

    Update key in events

--- app/core/events.py
+++ app/core/events.py
@@ -12,11 +12,11 @@ class NewAppProject(RequestEvent):
     app_directory = t.StringType(required=True)
     class Options():
         roles = {
-            'app_project.map': blacklist('app_key')
+            'app_project.map': blacklist('key')
         }
 
 class SetDefaultAppProject(RequestEvent):
-    app_key = t.StringType(required=True)
+    key = t.StringType(required=True)
 
 class SyncAppProject(RequestEvent):
     key = t.StringType(required=True)
