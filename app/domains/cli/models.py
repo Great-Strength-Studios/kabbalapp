2a800bd5f29705548a87b4d7c230fbc7b00cbb61
ashatz <andrew@greatstrength.me>
2023-04-27 06:53:36 -0700

    Add domain models for cli

--- app/domains/cli/models.py
+++ app/domains/cli/models.py
@@ -2,3 +2,37 @@ from typing import List
 from schematics import types as t, Model
 from schematics.transforms import whitelist, blacklist
 from schematics.types.serializable import serializable
+
+class AppArgument(Model):
+    name_or_flags = t.ListType(t.StringType(), required=True)
+    help = t.StringType(required=True)
+    type = t.StringType(default='str', choices=['str', 'int', 'float'])
+    default = t.StringType()
+    required = t.BooleanType()
+    nargs = t.StringType()
+    choices = t.ListType(t.StringType())
+    action = t.StringType()
+
+class AppSubcommand(Model):
+    help = t.StringType(required=True)
+    arguments = t.ListType(t.ModelType(AppArgument), default=[])
+
+    class Options():
+        serialize_when_none = False
+        roles = {
+            'add_subparser': blacklist('arguments')
+        }
+
+class AppCommand(Model):
+    key = t.StringType(required=True)
+    subcommands = t.DictType(t.ModelType(AppSubcommand), default=[])
+
+    class Options():
+        serialize_when_none = False
+        roles = {
+            'cli.add_command': blacklist('key', 'commands', 'parent_arguments')
+        }
+
+class AppCommands(Model):
+    parent_arguments = t.ListType(t.ModelType(AppArgument), default=[])
+    commands = t.DictType(t.ModelType(AppCommand), default={})
