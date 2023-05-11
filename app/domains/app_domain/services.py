d5bb73430d4301a946c5f4a84747a95d1cd753c2
ashatz <andrew@greatstrength.me>
2023-05-11 06:45:44 -0700

    Update domain model to blacklist properties

--- app/domains/app_domain/services.py
+++ app/domains/app_domain/services.py
@@ -23,6 +23,9 @@ class AppDomainService():
     def add_model(self, domain_key: str, key: str, name: str, class_name: str) -> AppDomainModel:
         pass
 
+    def get_models(self, domain_key: str = None) -> List[AppDomainModel]:
+        pass
+
     def get_model(self, domain_key: str, key: str) -> AppDomainModel:
         pass
 
