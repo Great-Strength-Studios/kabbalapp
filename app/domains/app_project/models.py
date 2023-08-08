1f32a38c8524db9999e7dfab74f9a9a4bd3adb22
ashatz <andrew@greatstrength.me>
2023-08-08 16:30:11 -0500

    Remove default version

--- app/domains/app_project/models.py
+++ app/domains/app_project/models.py
@@ -6,5 +6,5 @@ class AppProject(Model):
     name = t.StringType(required=True)
     app_directory = t.StringType(required=True)
     schema_storage_type = t.StringType(choices=['yaml'], default='yaml')
-    schema_location = t.StringType(required=True, default='app/schema.yaml')
-    version = t.StringType(required=True, default='0.0.1')
+    schema_location = t.StringType(required=True, default='app/schema.yml')
+    version = t.StringType(required=True)
