82ea7753a969a7f4df2e0faa30f38c342f5887a9
ashatz <andrew@greatstrength.me>
2023-09-18 17:00:03 -0700

    Add modules to add and remove extra stuff

--- app/features/app_project/sync_app_project.py
+++ app/features/app_project/sync_app_project.py
@@ -22,56 +22,26 @@ def handle(context: MessageContext):
         'app/__init__.py'
     ]
 
+    # Arrange packages/modules to be written to the domain package if they do not already exist.
+    add_domain_modules = [
+        'modules/__init__.py',
+        'repo/__init__.py',
+        '__init__.py',
+        'entities.py',
+        'factory.py',
+        'value_objects.py',
+    ]
+
+    # Arrange packages/modules to be written to the interfaces package if they do not already exist.
+    add_interface_modules = [
+        '__init__.py',
+        'commands.py',
+        'services.py',
+    ]
+
     # Load blocks.
     blocks = [app_printer.read_block(module) for module in modules]
 
     # Write blocks.
     for block in blocks:
         target_app_printer.print_block(block)
-
-    # Update project app config.
-    import os, yaml
-    app_project_manager: p.AppProjectManager = context.services.app_project_manager()
-    app_project = app_project_manager.load_project(request.key)
-
-    app_config = os.path.join(app_project.app_directory, 'app/app.yml')
-    with open(app_config) as stream:
-        app_config_data = yaml.safe_load(stream)
-
-    modules_old = app_config_data.get('endpoints', None)
-    # Exit this part if there are no endpoints (old format)
-    if not modules_old:
-        return
-    
-    modules_new = {}
-    for key, value in modules_old.items():
-        module_name, feature = key.split('.')
-        functions = []
-        try:
-            for function in value.get('modules'):
-                subdomain = function.pop('subdomain')
-                module = function.pop('module')
-                function_path = '{}.{}'.format(subdomain, module)
-                function['function_path'] = function_path
-                functions.append(function) 
-        except TypeError:
-            subdomain = value.pop('subdomain')
-            module = value.pop('module')
-            function_path = '{}.{}'.format(subdomain, module)
-            value['function_path'] = function_path
-            functions = [value]
-        try:
-            feature_data = {'functions': functions}
-            modules_new[module_name]['features'][feature] = feature_data
-            continue
-        except KeyError:
-            pass
-        try:
-            module_data = {'features': {feature: {'functions': functions}}}
-            modules_new[module_name] = module_data
-        except KeyError:
-            pass
-    app_config_data['modules'] = modules_new
-    app_config_data['endpoints'] = None
-    with open(app_config, 'w') as stream:
-        yaml.safe_dump(app_config_data, stream, default_flow_style=False)
