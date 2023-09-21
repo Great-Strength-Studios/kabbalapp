6226d69eef5b1b099a2904837c95104ba311e7c2
ashatz <andrew@greatstrength.me>
2023-09-20 22:32:38 -0700

    Fix more bugs

--- app/features/app_project/sync_app_project.py
+++ app/features/app_project/sync_app_project.py
@@ -42,7 +42,7 @@ def handle(context: MessageContext):
             """from schematics import types as t, Model
             from schematics.transforms import whitelist, blacklist
             from schematics.types.serializable import serializable""",
-        'interfaces/__init__.py': """from .comands import *""",
+        'interfaces/__init__.py': """from .commands import *""",
         'interfaces/commands.py': 
             """from schematics import types as t, Model
             from schematics.transforms import blacklist, whitelist""",
