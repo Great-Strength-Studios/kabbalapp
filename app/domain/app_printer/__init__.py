ff1356461b9e27a032e17986eb42955f98153e69
ashatz <andrew@greatstrength.me>
2023-09-20 22:05:32 -0700

    Check if block exists

--- app/domain/app_printer/__init__.py
+++ app/domain/app_printer/__init__.py
@@ -117,6 +117,11 @@ class AppPrinter(object):
             'code_block': code_block,
             'code_lines': code_block.splitlines()
         })
+    
+    def block_exists(self, file_path: str, base_path: str = None) -> bool:
+        if base_path:
+            file_path = os.path.join(base_path, file_path)
+        return os.path.exists(os.path.join(self.app_path, file_path))
         
     def read_block(self, file_path: str, base_path: str) -> AppModuleBlock:
         if base_path:
