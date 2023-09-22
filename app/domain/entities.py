07ac32bfc30b5019460990a0500112cf9b688974
ashatz <andrew@greatstrength.me>
2023-09-21 20:42:49 -0700

    Fix imports for entities

--- app/domain/entities.py
+++ app/domain/entities.py
@@ -1,3 +1,1 @@
-from schematics import types as t, Model
-from schematics.transforms import whitelist, blacklist
-from schematics.types.serializable import serializable
+from .value_objects import *
