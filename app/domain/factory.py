46695677c37583a7c0d057fbfa1dcc292aa213dc
ashatz <andrew@greatstrength.me>
2023-09-21 21:08:57 -0700

    Nvm, using static method instead

--- app/domain/factory.py
+++ app/domain/factory.py
@@ -1,9 +1,3 @@
 from .value_objects import *
 from .entities import *
 from .modules import *
-
-def create_cli_interface() -> cli.CliAppInterface:
-    interface = cli.CliAppInterface()
-    interface.type = 'cli'
-
-    return interface
