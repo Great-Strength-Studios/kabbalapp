87268e4d38e1cbf12b790c5eda206289cc1e54e9
ashatz <andrew@greatstrength.me>
2023-09-22 10:13:13 -0700

    Add initial models and app config

--- app/domain/modules/cli.py
+++ app/domain/modules/cli.py
@@ -1,7 +1,53 @@
 from ..entities import *
 
-# Dependencies
+# Module dependencies
 from . import i
+
+
+class CliArgument(Model):
+    key = t.StringType(required=True)
+    name_or_flags = t.ListType(t.StringType(), required=True)
+    help = t.StringType(required=True)
+    type = t.StringType(choices=['str', 'int', 'float'])
+    default = t.StringType()
+    required = t.BooleanType()
+    nargs = t.StringType()
+    choices = t.ListType(t.StringType())
+    action = t.StringType()
+
+    class Options():
+        serialize_when_none = False
+        roles = {
+            'cli.add_argument': blacklist('key'),
+            'cli.add_parent_argument': blacklist('key'),
+        }
+
+class CliSubcommand(Model):
+    key = t.StringType()
+    name = t.StringType(required=True)
+    help = t.StringType(required=True)
+    arguments = t.DictType(t.ModelType(CliArgument), default={})
+
+    class Options():
+        serialize_when_none = False
+        roles = {
+            'cli.add_subcommand': blacklist('key', 'arguments')
+        }
+
+class AppCommand(Model):
+    key = t.StringType(required=True)
+    subcommands = t.DictType(t.ModelType(CliSubcommand), default={})
+
+    class Options():
+        serialize_when_none = False
+        roles = {
+            'cli.add_command': blacklist('key', 'commands', 'parent_arguments')
+        }
+
+class AppCommands(Model):
+    parent_arguments = t.DictType(t.ModelType(CliArgument), default={})
+    commands = t.DictType(t.ModelType(AppCommand), default={})
+
 
 class CliAppInterface(i.AppInterface):
     type = t.StringType(required=True, choices=['cli'])
