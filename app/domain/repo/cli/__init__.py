2faef3a575ddda81a12457a180eff2642f0409b9
ashatz <andrew@greatstrength.me>
2023-09-22 16:46:13 -0700

    Add get_command method to interface

--- app/domain/repo/cli/__init__.py
+++ app/domain/repo/cli/__init__.py
@@ -6,6 +6,9 @@ class CliInterfaceRepository():
     def add_command(self, command: cli.CliCommand) -> cli.CliCommand:
         pass
 
+    def get_command(self, command_key: str, subcommand_key: str) -> cli.CliCommand:
+        pass
+
     def add_parent_argument(self, argument: cli.CliArgument) -> cli.CliArgument:
         pass
 
