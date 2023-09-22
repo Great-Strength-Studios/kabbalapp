f031cb951bbe2958d41aed1668fe5dcae4994fb8
ashatz <andrew@greatstrength.me>
2023-09-22 16:59:47 -0700

    Remove cli import from domain

--- app/domain/__init__.py
+++ app/domain/__init__.py
@@ -1,7 +1,6 @@
 from . import app_project, app_project as aproj, app_project as p
 from .app_printer import AppBlock, AppPrinter
 from . import app_domain, app_domain as adom, app_domain as d
-from . import cli
 
 from .value_objects import *
 from .entities import *
