fce1a9ea8307b5c6e28f825cdac2e92a91445cb2
ashatz <andrew@greatstrength.me>
2023-09-22 14:00:00 -0700

    Update repo

---
+++ app/domain/repo/cli/__init__.py
@@ -0,0 +1,13 @@
+from ...entities import *
+from ...modules import *
+
+class CliInterfaceService():
+
+    def add_command(self, command: cli.CliCommand) -> cli.CliCommand:
+        pass
+
+    def add_parent_argument(self, argument: cli.CliArgument) -> cli.CliArgument:
+        pass
+
+    def add_argument(self, command_key: str, argument: cli.CliArgument, subcommand_key: str = None) -> cli.CliArgument:
+        pass
