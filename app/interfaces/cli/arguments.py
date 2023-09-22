dcd53f34a588fe50f281bb7a39ae4863709b6669
ashatz <andrew@greatstrength.me>
2023-09-22 05:37:16 -0700

    Migrate configs to seperate module

--- app/interfaces/cli/arguments.py
+++ app/interfaces/cli/arguments.py
@@ -1,70 +1,12 @@
 import sys, argparse
-
 import yaml
 
-from schematics import types as t, Model
-from schematics.transforms import blacklist, whitelist
-from schematics.types.serializable import serializable
+from .config import *
 
-class AppArgument(Model):
-    name_or_flags = t.ListType(t.StringType(), required=True)
-    help = t.StringType(required=True)
-    type_str = t.StringType(default='str', choices=['str', 'int', 'float'])
-    default = t.StringType()
-    required = t.BooleanType()
-    nargs = t.StringType()
-    choices = t.ListType(t.StringType())
-    action = t.StringType()
-
-    @serializable
-    def type(self):
-        if self.action in ['store_true', 'store_false']:
-            return None
-        if self.type_str == 'str':
-            return str
-        elif self.type_str == 'int':
-            return int
-        elif self.type_str == 'float':
-            return float
-        else:
-            raise Exception('Invalid type')
-    
-    class Options():
-        serialize_when_none = False
-        roles = {
-            'add_argument': blacklist('type_str', 'name_or_flags')
-        }
-
-class AppSubcommand(Model):
-    name = t.StringType(required=True)
-    help = t.StringType(required=True)
-    arguments = t.DictType(t.ModelType(AppArgument), default={})
-
-    class Options():
-        serialize_when_none = False
-        roles = {
-            'add_subparser': blacklist('arguments')
-        }
-
-class AppCommand(Model):
-    help = t.StringType(required=True)
-    cli_subcommands = t.DictType(t.ModelType(AppSubcommand), default={})
-
-    class Options():
-        serialize_when_none = False
-        roles = {
-            'add_parser': blacklist('cli_subcommands')
-        }
-
-class AppCommands(Model):
-    type = t.StringType(required=True)
-    parent_arguments = t.DictType(t.ModelType(AppArgument), default={})
-    mappers = t.DictType(t.StringType())
-    cli_commands = t.DictType(t.ModelType(AppCommand), default={})
 
 with open('app/app.yml', 'r') as f:
     app_config = yaml.safe_load(f.read())
-commands = AppCommands([i for i in app_config['interfaces'] if i['type'] == 'cli'][0])
+commands = AppCommandsConfiguration([i for i in app_config['interfaces'] if i['type'] == 'cli'][0])
 
 # Create parser.
 parser = argparse.ArgumentParser()
