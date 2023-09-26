1814ea1d50823104a4348ad4defef72f6f6b8215
ashatz <andrew@greatstrength.me>
2023-09-26 12:51:00 -0700

    Update add parent argument module

--- app/features/cli/add_parent_argument.py
+++ app/features/cli/add_parent_argument.py
@@ -15,7 +15,6 @@ def handle(context: MessageContext):
     nargs = context.data.nargs
     action = context.data.action
 
-
     # Get app key from headers.
     app_key = context.headers.get('app_key', None)
 
@@ -26,25 +25,8 @@ def handle(context: MessageContext):
     # Get cli inteface repo.
     cli_interface_repo: CliInterfaceRepository = context.services.cli_interface_repo(app_key)
 
-    # Get cli parent command argument name or flags.
-    name_or_flags = []
-    if not positional:
-        name = '--{}'.format(name.lower().replace('_', '-').replace(' ', '-'))
-        if flags:
-            flags = ['-{}'.format(flag.replace('_', '-')) for flag in flags]
-    name_or_flags.append(name)
-    if flags:
-        name_or_flags.extend(flags)
-
     # Get cli interface type.
     interface = cli_interface_repo.get_inteface()
-
-    # Check to see if argument already exists.
-    exists = interface.parent_argument_exists(name_or_flags)
-
-    # Raise parent argument exists error if parent argument already exists.
-    if exists:
-        raise AppError(context.errors.CLI_ARGUMENT_ALREADY_EXISTS.format_message(str(name_or_flags)))
     
     # Create new argument instance.
     argument = CliArgument.create(
@@ -59,6 +41,13 @@ def handle(context: MessageContext):
         nargs=nargs,
         action=action
     )
+
+    # Check to see if argument already exists.
+    exists = interface.parent_argument_exists(argument)
+
+    # Raise parent argument exists error if parent argument already exists.
+    if exists:
+        raise AppError(context.errors.CLI_ARGUMENT_ALREADY_EXISTS.format_message(str(argument.name_or_flags)))
 
     # Add argument to interface.
     interface.add_parent_argument(argument)
