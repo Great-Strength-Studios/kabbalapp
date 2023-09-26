ccb051d6e4d84f3e6fe37c94bcc2c8be6e63135d
ashatz <andrew@greatstrength.me>
2023-09-26 09:51:39 -0700

    Update add parent argument module

--- app/features/cli/add_parent_argument.py
+++ app/features/cli/add_parent_argument.py
@@ -4,7 +4,17 @@ from ...domain import *
 def handle(context: MessageContext):
 	
     # Unpack request.
-    request = context.data
+    name = context.data.name
+    help = context.data.help
+    type = context.data.type
+    flags = context.data.flags
+    positional = context.data.positional
+    default = context.data.default
+    required = context.data.required
+    choices = context.data.choices
+    nargs = context.data.nargs
+    action = context.data.action
+
 
     # Get app key from headers.
     app_key = context.headers.get('app_key', None)
@@ -13,29 +23,46 @@ def handle(context: MessageContext):
     if not app_key:
         raise AppError(context.errors.APP_KEY_REQUIRED)
     
-    # Get cli inteface service.
-    service: cli.CliInterfaceService = context.services.cli_interface_service(app_key)
-
-    # Format key if none is provided.
-    if not request.key:
-        request.key = request.name.lower().replace(' ', '_')
+    # Get cli inteface repo.
+    cli_interface_repo: CliInterfaceRepository = context.services.cli_interface_repo(app_key)
 
     # Get cli parent command argument name or flags.
     name_or_flags = []
-    if not request.positional:
-        request.name = '--{}'.format(request.name.lower().replace('_', '-').replace(' ', '-'))
-        if request.flags:
-            request.flags = ['-{}'.format(flag.replace('_', '-')) for flag in request.flags]
-    name_or_flags.append(request.name)
-    if request.flags:
-        name_or_flags.extend(request.flags)
+    if not positional:
+        name = '--{}'.format(name.lower().replace('_', '-').replace(' ', '-'))
+        if flags:
+            flags = ['-{}'.format(flag.replace('_', '-')) for flag in flags]
+    name_or_flags.append(name)
+    if flags:
+        name_or_flags.extend(flags)
 
-    # Add parent command argument to cli interface.
-    argument = service.add_parent_argument(name_or_flags=name_or_flags, **request.to_primitive('cli.add_parent_argument'))
+    # Get cli interface type.
+    interface = cli_interface_repo.get_inteface()
 
-    # If cli parent command argument already exists, raise exception.
-    if isinstance(argument, tuple):
-        raise AppError(context.errors.get(argument[0]).format_message(*argument[1:]))
+    # Check to see if argument already exists.
+    exists = interface.parent_argument_exists(name_or_flags)
+
+    # Raise parent argument exists error if parent argument already exists.
+    if exists:
+        raise AppError(context.errors.CLI_ARGUMENT_ALREADY_EXISTS.format_message(str(name_or_flags)))
     
-    # Return cli parent command argument.
+    # Create new argument instance.
+    argument = CliArgument.create(
+        name_or_flags=name_or_flags,
+        help=help,
+        type=type,
+        default=default,
+        required=required,
+        choices=choices,
+        nargs=nargs,
+        action=action
+    )
+
+    # Add argument to interface.
+    interface.add_parent_argument(argument)
+
+    # Save interface.
+    cli_interface_repo.save_interface(interface)
+
+    # Return added argument.
     return argument
