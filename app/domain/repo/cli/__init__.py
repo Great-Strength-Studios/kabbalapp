6335977547ab515e9cf63839035dc9ff0a361be7
ashatz <andrew@greatstrength.me>
2023-09-26 09:00:31 -0700

    Update cli interface repo interface

--- app/domain/repo/cli/__init__.py
+++ app/domain/repo/cli/__init__.py
@@ -3,14 +3,8 @@ from ...modules import *
 
 class CliInterfaceRepository():
 
-    def add_command(self, command: cli.CliCommand) -> cli.CliCommand:
+    def get_inteface(self) -> CliInterfaceType:
         pass
 
-    def get_command(self, command_key: str, subcommand_key: str) -> cli.CliCommand:
-        pass
-
-    def add_parent_argument(self, argument: cli.CliArgument) -> cli.CliArgument:
-        pass
-
-    def add_argument(self, command_key: str, argument: cli.CliArgument, subcommand_key: str = None) -> cli.CliArgument:
+    def save_interface(self, argument: CliInterfaceType) -> CliInterfaceType:
         pass
