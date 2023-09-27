de6f7f8ea809de47eed60c209d95c646d7355878
ashatz <andrew@greatstrength.me>
2023-09-27 12:35:43 -0700

    Move value object to entities module

--- app/domain/entities.py
+++ app/domain/entities.py
@@ -1,1 +1,6 @@
 from .value_objects import *
+
+class ValueObject(Model):
+    id = t.StringType(required=True)
+    name = t.StringType(required=True)
+    class_name = t.StringType(required=True)
--- app/domain/modules/domain.py
+++
@@ -1,6 +0,0 @@
-from ..entities import *
-
-class ValueObject(Model):
-    id = t.StringType(required=True)
-    name = t.StringType(required=True)
-    class_name = t.StringType(required=True)
