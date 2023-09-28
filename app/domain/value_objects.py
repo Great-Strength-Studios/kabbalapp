1e4de02122028d2f1c4bdd9fbddb4307bdc57797
ashatz <andrew@greatstrength.me>
2023-09-28 13:29:28 -0700

    Add factory method to model property

--- app/domain/value_objects.py
+++ app/domain/value_objects.py
@@ -3,6 +3,26 @@ from schematics import types as t, Model
 from schematics.transforms import blacklist, whitelist
 from schematics.types.serializable import serializable
 
+from ..constants import *
+
 
 class ModelProperty(Model):
-	pass
+	
+	name = t.StringType(required=True)
+	type = t.StringType(required=True, choices=DOMAIN_PROPERTY_TYPES)
+	required = t.BooleanType()
+	default = t.StringType()
+	choices = t.ListType(t.StringType())
+	description = t.StringType()
+
+	@staticmethod
+	def create(name: str, type: str = 'str', required: bool = False, default: str = None, choices: List[str] = [], description: str = None) -> 'ModelProperty':
+		result = ModelProperty()
+		result.name = name
+		result.type = type
+		result.required = required
+		result.default = default
+		result.choices = choices
+		result.description = description
+
+		return result
