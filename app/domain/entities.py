f107e9473dd1449f88709394d336d9f0c8a45331
ashatz <andrew@greatstrength.me>
2023-09-27 17:46:13 -0700

    Create value object static method.

--- app/domain/entities.py
+++ app/domain/entities.py
@@ -5,3 +5,18 @@ class ValueObject(Model):
     name = t.StringType(required=True)
     class_name = t.StringType(required=True)
     properties = t.ListType(t.StringType(), default=[])
+
+    @staticmethod
+    def create(name: str, class_name: str, id: str = None) -> 'ValueObject':
+        result = ValueObject()
+        result.name = name
+        result.class_name = class_name
+
+        # Convert python camel case to snake case if ID is not provided
+        if id is None:
+            import re
+            result.id = name = re.sub(r'(?<!^)(?=[A-Z])', '_', class_name).lower()
+        else:
+            result.id = id
+
+        return result
