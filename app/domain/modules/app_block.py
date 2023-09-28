d189ee5a9dd878f90d734a1b12bd423bc1be460c
ashatz <andrew@greatstrength.me>
2023-09-28 12:36:21 -0700

    Add model property block

--- app/domain/modules/app_block.py
+++ app/domain/modules/app_block.py
@@ -10,7 +10,55 @@ class ModelPropertyBlock(Model):
         result.property = property
 
         return result
+    
+    def print_lines(self):
+        # Create empty list representing print lines
+        print_lines = []
 
+        # Create the initial string with the tab indent.
+        property_str = '\t'
+
+        # Add the name
+        property_str += f'{self.property.name} = '
+
+        # Add the type
+        if self.property.type == 'int':
+            type_name = 'Int'
+        elif self.property.type == 'float':
+            type_name = 'Float'
+        property_str += f't.{type_name}Type('
+
+        # Create empty list for type arguments
+        type_args = []
+
+        # Add the required flag
+        if self.property.required:
+            type_args.append('required=True')
+        
+        # Add the default flag
+        if self.property.default is not None:
+            type_args.append(f'default={self.property.default}')
+
+        # Add the choices flag
+        if len(self.property.choices) > 0:
+            type_args.append(f'choices={str(self.property.choices)}')
+        
+        # Add the type args to the property string
+        if len(type_args) > 0:
+            property_str += ', '.join(type_args)
+        
+        # Close the type
+        property_str += ')'
+
+        # Add the property string to the print lines
+        print_lines.append(property_str)
+
+        # Return list
+        return print_lines
+    
+    def print(self):
+        return '\n'.join(self.print_lines())
+        
 
 class ValueObjectBlock(Model):
 
@@ -60,6 +108,9 @@ class ValueObjectBlock(Model):
                 print_lines.append('\tpass')
             
             # Otherwise, add the properties
+            for property in value_object.properties:
+                property_block = ModelPropertyBlock.create(property)
+                print_lines.extend(property_block.print_lines())
             
             # Increment the counter
             i += 1
