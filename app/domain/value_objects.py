538cfef70ab30258122910c71eed69e7d48d0c79
ashatz <andrew@greatstrength.me>
2023-09-28 12:07:38 -0700

    Add model property to value object

--- app/domain/value_objects.py
+++ app/domain/value_objects.py
@@ -2,3 +2,7 @@ from typing import List
 from schematics import types as t, Model
 from schematics.transforms import blacklist, whitelist
 from schematics.types.serializable import serializable
+
+
+class ModelProperty(Model):
+	pass
