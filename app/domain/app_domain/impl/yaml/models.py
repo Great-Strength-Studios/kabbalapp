3085d685d614f43e1745cea75015b2cfcfd860aa
ashatz <andrew@greatstrength.me>
2023-09-29 01:57:24 -0700

    Remove legacy domain property model stuff

--- app/domain/app_domain/impl/yaml/models.py
+++ app/domain/app_domain/impl/yaml/models.py
@@ -1,9 +1,1 @@
 from ...models import *
-
-class YamlAppDomainModelProperty(AppDomainModelProperty):
-
-    class Options():
-        serialize_when_none = False,
-        roles = {
-            'domain.add_property': blacklist('key')
-        }
