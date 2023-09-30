9e1416f3ecd6a5ba01adb95d4d718ed6575c475b
ashatz <andrew@greatstrength.me>
2023-09-30 00:51:39 -0700

    Add type property factory

--- app/domain/factory.py
+++ app/domain/factory.py
@@ -1,3 +1,9 @@
 from .value_objects import *
 from .entities import *
 from .modules import *
+
+def create_type_properties(type: str, **properties)-> TypeProperties:
+    if type == 'str':
+        return StringTypeProperties.create(**properties)
+    
+    return None
