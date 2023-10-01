5aaefb7c2af69c041c3eaed93a1e49d5d62b53b1
ashatz <andrew@greatstrength.me>
2023-09-30 22:17:42 -0700

    Remove generic interface service method

--- app/domain/repo/interface/__init__.py
+++ app/domain/repo/interface/__init__.py
@@ -5,6 +5,3 @@ class AppInterfaceRepository():
 
     def get_interfaces(self) -> AppInterfaceType:
         pass
-
-    def save_interface_type(self, interface: AppInterfaceType) -> None:
-        pass
