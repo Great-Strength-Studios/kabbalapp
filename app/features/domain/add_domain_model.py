e3130491000c016580fc593f81b6062cad59994c
ashatz <andrew@greatstrength.me>
2023-10-02 11:22:33 -0700

    Update module

--- app/features/domain/add_domain_model.py
+++ app/features/domain/add_domain_model.py
@@ -6,6 +6,7 @@ def handle(context: MessageContext):
     # Unpack request.
     name = context.data.name
     class_name = context.data.class_name
+    type = context.data.type
 
     # Get app project key.
     app_key = context.headers.get('app_key', None)
@@ -17,22 +18,26 @@ def handle(context: MessageContext):
     # Get domain repository.
     domain_repo: DomainRepository = context.services.domain_repo(app_key)
 
-    # Get all value objects.
-    value_objects = domain_repo.get_value_objects()
+    # Execute the following if the input type is a value object
+    if type == 'value_object':
 
-    # Create new value object instance.
-    value_object = AppValueObject.create(name=name, class_name=class_name)
+        # Get all value objects.
+        value_objects = domain_repo.get_value_objects()
 
-    # Check to see if value object already exists.
-    exists = next((vo for vo in value_objects if vo.id == value_object.id), None)
+        # Create new value object instance.
+        domain_model = AppValueObject.create(name=name, class_name=class_name)
+
+        # Check to see if value object already exists.
+        exists = next((vo for vo in value_objects if vo.id == domain_model.id), None)
     
-    # Raise Value Object already exists error if value object already exists.
-    if exists:
-        raise AppError(context.errors.VALUE_OBJECT_ALREADY_EXISTS.format_message(value_object.class_name))
+        # Raise Value Object already exists error if value object already exists.
+        if exists:
+            raise AppError(context.errors.VALUE_OBJECT_ALREADY_EXISTS.format_message(domain_model.class_name))
 
-    # Add value object to domain and to value objects list.
-    domain_repo.save_value_object(value_object)
-    value_objects.append(value_object)
+        # Add value object to domain and to value objects list.
+        domain_repo.save_value_object(domain_model)
+        value_objects.append(domain_model)
+    
 
     # Get app project manager.
     app_project_manager: p.AppProjectManager = context.services.app_project_manager()
@@ -40,12 +45,15 @@ def handle(context: MessageContext):
     # Get app project.
     app_project = app_project_manager.load_project(app_key)
 
-    # Create value object block.
-    value_object_block = ValueObjectBlock.create(app_project.app_directory, value_objects)
+    # Run the following code if the input type is a value object.
+    if type == 'value_object':
 
-    # Add value object block to app project.
-    with open(value_object_block.file_path, 'w') as f:
-        f.write(value_object_block.print())
+        # Create value object block.
+        block = ValueObjectBlock.create(app_project.app_directory, value_objects)
+
+    # Add domain model block to app project.
+    with open(block.file_path, 'w') as f:
+        f.write(block.print())
 
     # Return added value object.
-    return value_object
+    return domain_model
