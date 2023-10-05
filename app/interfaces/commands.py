332a531d60af7af441b2db7b3e57e8ed0a1e8af2
ashatz <andrew@greatstrength.me>
2023-10-05 13:09:12 -0700

    Remove implementation commands and mappings

--- app/interfaces/commands.py
+++ app/interfaces/commands.py
@@ -63,16 +63,6 @@ class AddCliArgument(RequestEvent):
             'cli.add_argument': blacklist('name', 'flags', 'positional'),
         }
 
-class AddDomainImplementation(RequestEvent):
-    domain_key = t.StringType(required=True)
-    key = t.StringType()
-    name = t.StringType(required=True)
-
-class SyncDomainImplementation(RequestEvent):
-    domain_key = t.StringType(required=True)
-    implementation_key = t.StringType(required=True)
-    force = t.BooleanType(default=False)
-
 class AddDomainModel(RequestEvent):
     name = t.StringType(required=True)
     type = t.StringType(required=True)
