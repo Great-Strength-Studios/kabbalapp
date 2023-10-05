16d68bf986759e5284fe48429a621b8b31ed3038
ashatz <andrew@greatstrength.me>
2023-10-05 13:13:17 -0700

    Remove model and services

--- app/domain/app_domain/models.py
+++ app/domain/app_domain/models.py
@@ -5,20 +5,6 @@ from schematics.types.serializable import serializable
 
 from ...constants import *
 
-class AppDomainModel(Model):
-    key = t.StringType(required=True)
-    name = t.StringType(required=True)
-    class_name = t.StringType(required=True)
-
-    class Options():
-        serialize_when_none = False
-        roles = {
-            'domain.add_model': blacklist('key'),
-            'domain.add_property': blacklist('key'),
-            'domain.get_domain': blacklist(),
-            'domain.list_domain_models': blacklist(),
-        }
-
 class AppDomainRole(Model):
     type = t.StringType(required=True)
     fields = t.ListType(t.StringType(), required=True)
--- app/domain/app_domain/services.py
+++ app/domain/app_domain/services.py
@@ -2,14 +2,5 @@ from .models import *
 
 class AppDomainService():
 
-    def add_model(self, domain_key: str, key: str, name: str, class_name: str) -> AppDomainModel:
-        pass
-
-    def get_models(self, domain_key: str = None) -> List[AppDomainModel]:
-        pass
-
-    def get_model(self, domain_key: str, key: str) -> AppDomainModel:
-        pass
-
     def add_role(self, domain_key: str, key: str, type: str, fields: List[str]) -> AppDomainRole:
         pass
