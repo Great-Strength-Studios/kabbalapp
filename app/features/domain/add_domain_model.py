4c4caec61bc139e727fcf2bda1439f750a493ff8
ashatz <andrew@greatstrength.me>
2023-10-06 19:14:45 -0700

    Update add domain module

--- app/features/domain/add_domain_model.py
+++ app/features/domain/add_domain_model.py
@@ -7,6 +7,7 @@ def handle(context: MessageContext):
     name = context.data.name
     class_name = context.data.class_name
     type = context.data.type
+    base_type_model_id = context.data.base_type_model_id
 
     # Get app project key.
     app_key = context.headers.get('app_key', None)
@@ -31,6 +32,19 @@ def handle(context: MessageContext):
     # Raise Value Object already exists error if value object already exists.
     if existing_model:
         raise AppError(context.errors.DOMAIN_MODEL_ALREADY_EXISTS.format_message(domain_model.type, domain_model.class_name))
+    
+    # If the type has a base type, first verify that the base type exists.
+    if base_type_model_id:
+        base_type = domain_repo.get_domain_model(base_type_model_id)
+        if not base_type:
+            raise AppError(context.errors.DOMAIN_MODEL_BASE_TYPE_NOT_FOUND.format_message(base_type_model_id))
+        # Add base type as dependency
+        dependency = d.DomainModelDependency.create(
+            model_id=base_type_model_id,
+            class_name=base_type.class_name,
+            dependency_type='base_type',
+        )
+        domain_model.add_dependency(dependency)
 
     # Add value object to domain and to value objects list.
     domain_repo.save_domain_model(domain_model)
