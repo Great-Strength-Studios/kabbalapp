3344ec3f33faf6411d44abc2d28f27f5027ebe19
ashatz <andrew@greatstrength.me>
2023-10-23 12:10:26 -0700

    Minor factory updates

--- app/domain/factory.py
+++ app/domain/factory.py
@@ -2,13 +2,13 @@ from .models import *
 from .modules import *
 
 def create_type_properties(type: str, **properties)-> TypeProperties:
-    if type == 'str':
+    if type == STR_TYPE:
         return StringTypeProperties.create(**properties)
-    elif type == 'list':
+    elif type == LIST_TYPE:
         return ListTypeProperties.create(**properties)
-    elif type == 'date':
+    elif type == DATE_TYPE:
         return DateTypeProperties.create(**properties)
-    elif type == 'datetime':
+    elif type == DATETIME_TYPE:
         return DateTimeTypeProperties.create(**properties)
     
     return None
