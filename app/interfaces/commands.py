e914237d686177633be3cd85e5617a51825b1bf6
ashatz <andrew@greatstrength.me>
2023-10-25 20:29:16 -0700

    Add Request Event Property - property_setting

--- app/interfaces/commands.py
+++ app/interfaces/commands.py
@@ -4,25 +4,35 @@ from schematics.transforms import blacklist, whitelist
 class RequestEvent(Model):
     pass
 
+
 class NewAppProject(RequestEvent):
+    
     name = t.StringType(required=True)
     tag = t.StringType(required=True)
     app_directory = t.StringType(required=True)
 
+
 class SetDefaultAppProject(RequestEvent):
+    
     tag = t.StringType(required=True)
 
+
 class AddInterface(RequestEvent):
+    
     name = t.StringType(required=True)
     type = t.StringType(required=True, choices=['cli', 'rest_flask'])
 
+
 class AddCliCommand(RequestEvent):
+    
     command_key = t.StringType(required=True)
     subcommand_key = t.StringType()
     name = t.StringType(required=True)
     help = t.StringType(required=True, deserialize_from=['help', 'description'])
 
+
 class AddCliParentArgument(RequestEvent):
+    
     name = t.StringType(required=True)
     help = t.StringType(required=True, deserialize_from=['help', 'description'])
     type = t.StringType(choices=['str', 'int', 'float'])
@@ -40,7 +50,9 @@ class AddCliParentArgument(RequestEvent):
             'cli.add_parent_argument': blacklist('name', 'flags', 'positional'),
         }
 
+
 class AddCliArgument(RequestEvent):
+    
     command_key = t.StringType(required=True)
     subcommand_key = t.StringType()
     name = t.StringType(required=True)
@@ -60,13 +72,16 @@ class AddCliArgument(RequestEvent):
             'cli.add_argument': blacklist('name', 'flags', 'positional'),
         }
 
+
 class AddDomainModel(RequestEvent):
     name = t.StringType(required=True)
     type = t.StringType(required=True)
     class_name = t.StringType()
     base_type_model_id = t.StringType()
 
+
 class AddDomainModelProperty(RequestEvent):
+    
     model_id = t.StringType(required=True)
     name = t.StringType(required=True)
     type = t.StringType(default='str')
@@ -79,13 +94,19 @@ class AddDomainModelProperty(RequestEvent):
     description = t.StringType()
     type_properties = t.DictType(t.StringType(), default={})
 
+
 class UpdateDomainModelProperty(RequestEvent):
+    
     model_id = t.StringType(required=True)
     property_name = t.StringType(required=True)
+    property_setting = t.StringType(required=True, choices=['name', 'required', 'default', 'choices', 'description'])
+
 
 class RemoveDomainModelProperty(RequestEvent):
+   
     model_id = t.StringType(required=True)
     property_name = t.StringType(required=True)
+    
 
 class PrintDomainModelModule(RequestEvent):
     pass
