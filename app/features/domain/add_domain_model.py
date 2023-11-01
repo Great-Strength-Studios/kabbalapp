6a7e0e46aadd982c6eb0221ef7fdbf204a3204b0
Andrew <andrew@greatstrength.me>
2023-10-31 22:40:18 -0700

    5427853167 -  Add Description to Domain Model (#68)
    * Misc - Remove Print after Model Add/Remove
    
    * Add CLI Argument - description
    
    * Add Feature Request - Add Domain Model
    
    * Add Feature Request Property - description
    
    * Feature - RM - U - add_domain_model
    
    * Feature - DF - U - add_domain_model
    
    * Feature - DF - U - add_domain_model (2)
    
    * (Extra) Domain - M - A - App Domain Model
    
    * Domain - MP - A - description
    
    * (Extra) Domain - MFM - A - create
    
    * Domain - MFMP - A - description
    
    * Domain - Add property and factory parameter
    
    * Misc - Fix minor formatting
    
    * Add property to command

--- app/features/domain/add_domain_model.py
+++ app/features/domain/add_domain_model.py
@@ -3,12 +3,6 @@ from ...domain import *
 
 def handle(context: MessageContext):
     
-    # Unpack request.
-    name = context.data.name
-    class_name = context.data.class_name
-    type = context.data.type
-    base_type_model_id = context.data.base_type_model_id
-
     # Get app project key.
     app_key = context.headers.get('app_key', None)
 
@@ -20,33 +14,39 @@ def handle(context: MessageContext):
     domain_repo: d.DomainRepository = context.services.domain_repo(app_key)
 
     # Create new value object instance.
-    domain_model = d.AppDomainModel.create(
-        name=name, 
-        type=type, 
-        class_name=class_name,
-        base_type_model_id=base_type_model_id
-    )
+    domain_model = d.AppDomainModel.create(**context.data.to_primitive())
 
     # Check to see if existing domain model exists.
     existing_model = domain_repo.get_domain_model(domain_model.id)
 
     # Raise Value Object already exists error if value object already exists.
     if existing_model:
-        raise AppError(context.errors.DOMAIN_MODEL_ALREADY_EXISTS.format_message(domain_model.type, domain_model.class_name))
+        raise AppError(
+            context.errors.DOMAIN_MODEL_ALREADY_EXISTS.format_message(
+                domain_model.type, 
+                domain_model.class_name
+        ))
     
     # If the type has a base type, first verify that the base type exists.
-    if base_type_model_id:
-        base_type = domain_repo.get_domain_model(base_type_model_id)
+    if domain_model.base_type_model_id:
+        base_type = domain_repo.get_domain_model(domain_model.base_type_model_id)
         if not base_type:
-            raise AppError(context.errors.DOMAIN_MODEL_BASE_TYPE_NOT_FOUND.format_message(base_type_model_id))
+            raise AppError(
+                context.errors.DOMAIN_MODEL_BASE_TYPE_NOT_FOUND.format_message(
+                    domain_model.base_type_model_id
+            ))
         
         # Raise app error if the base type and the model type are not the same.
-        if base_type.type != type:
-            raise AppError(context.errors.DOMAIN_MODEL_INVALID_BASE_TYPE.format_message(base_type_model_id, type))
+        if base_type.type != domain_model.type:
+            raise AppError(
+                context.errors.DOMAIN_MODEL_INVALID_BASE_TYPE.format_message(
+                    domain_model.base_type_model_id, 
+                    domain_model.type
+            ))
         
         # Add base type as dependency
         dependency = d.DomainModelDependency.create(
-            model_id=base_type_model_id,
+            model_id=domain_model.base_type_model_id,
             class_name=base_type.class_name,
             dependency_type='base_type',
         )
