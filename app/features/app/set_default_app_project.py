3f4e11e513226e252650760456446a6bf1b47eac
ashatz <andrew@greatstrength.me>
2023-11-03 15:31:30 -0700

    Update set default app project

--- app/features/app/set_default_app_project.py
+++ app/features/app/set_default_app_project.py
@@ -2,19 +2,16 @@ from ...core import *
 from ...domain import *
 
 def handle(context: MessageContext):
-
-    # Unpack request.
-    tag = context.data.tag
     
     # Get app project manager.
     app_project_repo: app.AppProjectRepository = context.services.app_project_repo()
 
     # Verify that the app project exists.
-    if not app_project_repo.project_exists(tag):
-        raise AppError(context.errors.APP_PROJECT_NOT_FOUND.format_message(tag))
+    if not app_project_repo.project_exists(context.data.id):
+        raise AppError(context.errors.APP_PROJECT_NOT_FOUND.format_message(context.data.id))
 
     # Set default project.
-    app_project_repo.set_default_app_project(tag)
+    app_project_repo.set_default_app_project(context.data.id)
 
     # Return response.
-    return tag
+    return context.data.id
