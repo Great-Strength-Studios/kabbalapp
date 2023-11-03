630eb676ad90d60e731e87cd12275d5acfed81a1
ashatz <andrew@greatstrength.me>
2023-11-02 21:31:58 -0700

    Domain - DMP - Add - name_or_flags

--- app/domain/modules/cli/models.py
+++ app/domain/modules/cli/models.py
@@ -5,7 +5,7 @@ from .. import interface, interface as i
 
 
 class CliArgument(ValueObject):
-    name_or_flags = t.ListType(t.StringType(), required=True)
+    name_or_flags = t.ListType(t.StringType, required=True)
     help = t.StringType(required=True)
     type = t.StringType(choices=['str', 'int', 'float'])
     default = t.StringType()
@@ -82,7 +82,7 @@ class CliCommand(Entity):
 
 
 class CliInterfaceType(i.AppInterfaceType):
-    mappers = t.DictType(t.StringType())
+    mappers = t.DictType(t.StringType)
     commands = t.ListType(t.ModelType(CliCommand), default=[])
     parent_arguments = t.ListType(t.ModelType(CliArgument), default=[])
 
