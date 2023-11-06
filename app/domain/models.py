eae6190c3e55820805e43da2e7f4a34c94468b64
ashatz <andrew@greatstrength.me>
2023-11-05 20:28:55 -0700

    Domain - DM - Print (add create method)

--- app/domain/models.py
+++ app/domain/models.py
@@ -133,3 +133,23 @@ class DomainMethod(ValueObject):
     description = t.StringType()
     return_type = t.StringType(choices=['str', 'int', 'float', 'bool', 'date', 'datetime', 'model'])
     inner_return_type = t.StringType()
+
+    @staticmethod
+    def create(name: str, type: str, parent_id: str, description: str, return_type: str = None, inner_return_type: str = None) -> 'DomainMethod':
+
+        # Create new model instance.
+        result = DomainMethod()
+
+        # Load attributes from passed in values.
+        result.name = name
+        result.type = type
+        result.parent_id = parent_id
+        result.description = description
+        result.return_type = return_type
+        result.inner_return_type = inner_return_type
+
+        # Validate model instance.
+        result.validate()
+
+        # Return model instance.
+        return result
