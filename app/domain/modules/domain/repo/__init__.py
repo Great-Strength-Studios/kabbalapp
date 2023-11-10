047804c199a91563342c113af0dab800dcce6a1d
ashatz <andrew@greatstrength.me>
2023-11-10 15:00:41 -0700

    Print -t repo -p modules/domain/repo

--- app/domain/modules/domain/repo/__init__.py
+++ app/domain/modules/domain/repo/__init__.py
@@ -1,6 +1,8 @@
 from ..models import *
 
 class DomainRepository():
+    ''' The Domain Repository is responsible for managing the domain models, repositories, and modules for an app.
+    '''
 
     # TODO: Incorporate real domain model later.
     def get_domain_model(self, id: str) -> AppDomainModel:
@@ -10,4 +12,20 @@ class DomainRepository():
         pass
 
     def save_domain_model(self, domain_model: AppDomainModel) -> None:
+        pass
+
+    def get_repository(self, id: str) -> AppRepository:
+        '''Returns the repository with the input id.
+
+        :param id: The id of the repository to retrieve.
+        :type id: str
+        '''
+        pass
+
+    def save_repository(self, repository: AppRepository):
+        '''Saves the input repository.
+
+        :param repository: The repository to save.
+        :type repository: class: `domain.models.AppRepository`
+        '''
         pass
