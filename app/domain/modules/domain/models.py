4aef3c3d1f1e8f0c6c65f5f0b80c515e48c08e65
ashatz <andrew@greatstrength.me>
2023-12-18 10:47:39 -0700

    5667984989 - Remove Domain Model Attribute

--- app/domain/modules/domain/models.py
+++ app/domain/modules/domain/models.py
@@ -3,7 +3,6 @@ from ...models import *
 
 class DomainModelAttribute(Attribute):
 
-    inner_type_model_id = t.StringType()
     poly_type_model_ids = t.ListType(t.StringType())
 
     @staticmethod
