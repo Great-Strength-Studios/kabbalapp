e4701b9601d04183f9c9dc74816dae218fbfd377
ashatz <andrew@greatstrength.me>
2023-12-19 14:54:12 -0700

    5713654851 - Add Domain Model Attribute

--- app/domain/models.py
+++ app/domain/models.py
@@ -294,3 +294,4 @@ class Parameter(AppValueObject):
 
     type = t.StringType(required=True, choices=['str', 'int', 'float', 'bool', 'date', 'datetime', 'list', 'dict', 'model'])
     inner_type = t.StringType()
+    type_model_id = t.StringType()
